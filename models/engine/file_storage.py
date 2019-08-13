a368ab0585a9db32e9749ab3ae042f0d04ab0bde
jembantonio <jembantonio@gmail.com>
2019-08-13 17:24:18 +0000

    updated file_storage.py to delete obj from __objects if it is found

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,3 +55,11 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """deletes obj if its inside __objects
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            del self.__objects[key]
+            self.save()
