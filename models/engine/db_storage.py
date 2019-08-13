a95e156c9447c12a7e3019327d0806965aff3407
jembantonio <jembantonio@gmail.com>
2019-08-13 23:06:24 +0000

    updated db storage engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,6 +6,13 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 class DBStorage:
     '''Database Storage class
     '''
@@ -20,11 +27,9 @@ class DBStorage:
         host = os.getenv("HBNB_MYSQL_HOST")
         dbse = os.getenv("HBNB_MYSQL_DB")
 
-
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
             user, pswd, host, dbse), pool_pre_ping=True)
 
-        Base.metadata.create_all(self.__engine)
 
         if os.getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(bind=self.__engine)
@@ -36,15 +41,16 @@ class DBStorage:
         cls_list = ['User', 'State', 'City', 'Amenity', 'Place', 'Review']
 
         if cls is not None:
-            for cls_inst in self.__session.query(models.classes[cls]).all():
-                    for objects in cls_inst:
-                        cls_dict.append(cls_inst)
-
+            for cls_inst in self.__session.query(eval(cls).all()):
+                key = "{}.{}".format(type(obj).__name__, obj.id)
+                cls_dict[key] = obj
 
         else:
             for all_class in cls_list:
-                for cls_inst in self.__sesion.query(models.classes[all_class]).all():
-                    cls_dict.append(cls_inst)
+                for cls_inst in self.__sesion.query(all_class).all():
+                    key = "{}.{}".format(type(obj).__name__, obj.id)
+                    cls_dict[key] = obj
+
        # if cls is None:
         #    for cls_inst in self.__session.query(
          #       User, State, City, Amenity, Place, Review).all():
@@ -53,15 +59,17 @@ class DBStorage:
         return(cls_dict)
 
     def new(self, obj):
-        self.__session.add(obj)
+        if obj:
+            self.__session.add(obj)
 
     def save(self):
         self.__session.commit()
 
     def delete(self, obj=None):
-        for items_to_delete in obj:
-            self.__session.delete(items_to_delete)
+        if obj is not None:
+            self.__session.delete(obj)
 
     def reload(self):
-        Session = sessionmaker(bind=engine, expire_on_commit=False)
-        session = scoped_session(Session)
+        session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session)
+        self.__session = Session()
