e6656ead21da29f608b72a5b13a59dddf344c670
nokeefe <nokeefe1919@gmail.com>
2019-08-13 00:16:17 +0000

    adding dev user mysql script setup

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- prepares mysql server for hbnb project, dev db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- add new user hbnb_dev
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+-- grant all privileges on database hbnb_dev_db and
+GRANT ALL PRIVILEGES ON hbnb_dev_db.table TO 'hbnb_dev'@'localhost';
+-- SELECT privilege on performance_schema db
+GRANT SELECT PRIVILEGES ON performance_scheme TO 'hbnb_dev'@'localhost';
