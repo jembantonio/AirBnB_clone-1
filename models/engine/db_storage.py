d6ce623b7e9c8cb8a8be07f114baa0764ea35f4e
jembantonio <jembantonio@gmail.com>
2019-08-14 23:05:52 +0000

    changed db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -38,22 +38,17 @@ class DBStorage:
         '''queries on the current database session (self.__session)
         '''
         cls_dict = {}
-#        cls_list = ['User', 'State', 'City', 'Amenity', 'Place', 'Review']
 
-        if cls is not None:
-            #for cls_inst in self.__session.query(eval(cls)).all():
-                #for search in cls_inst:
-                    #cls_dict.append(search)
-            class_objects = self.__session.query(eval(cls)).all()
-            for c_object in class_objects:
-                key = type(c_object).__name__ + "." + str(c_object.id)
-                cls_dict[key] = c_object
+        if cls:
+            for cls_inst in self.__session.query(eval(cls).all()):
+                key = "{}.{}".format(type(cls_inst).__name__, cls_inst.id)
+                cls_dict[key] = cls_inst
 
         else:
-            class_objects = self.__session.query(State, City).all()
-            for c_object in class_objects:
-                key = type(c_object).__name__ + "." + str(c_object.id)
-                cls_dict[key] = c_object
+            for cls_inst in self.__session.query(State).all():
+                key = "{}.{}".format(type(cls_inst).__name__, cls_inst.id)
+                cls_dict[key] = cls_inst
+
         return cls_dict
 
     def new(self, obj):
