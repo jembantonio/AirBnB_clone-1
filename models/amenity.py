c08cdca86bcf4628d128000e48d12e5b3a032a67
nokeefe <nokeefe1919@gmail.com>
2019-08-15 23:18:58 +0000

    adding amenity file

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """This is the amenity class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey, Integer, Flaot
+from sqlalchemy.orm import relationship
+import os
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """This is the class for Amenity
     Attributes:
+        __tablename__: amenity table name
         name: input name
     """
-    name = ""
+    __tablename__ = "amenities"
+
+    name = Column(String(128), nullable=False)
+
+    place_amenities = relationship(
+                      "Place", backref="Amenity", cascade="all, delete-orphan")
