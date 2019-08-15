7aa7dc74e8beacc8c010eb1c8bc7224d4583e407
nokeefe <nokeefe1919@gmail.com>
2019-08-15 18:54:11 +0000

    removing whitespace

--- models/user.py
+++ models/user.py
@@ -2,8 +2,8 @@
 """This is the user class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
-import os
 from sqlalchemy.orm import relationship
+import os
 
 
 class User(BaseModel, Base):
@@ -16,9 +16,14 @@ class User(BaseModel, Base):
         last_name: last name
     """
     __tablename__ = "users"
+
     email = Column(String(128), nullable=False)
+
     password = Column(String(128), nullable=False)
+
     first_name = Column(String(128), nullable=True)
+
     last_name = Column(String(128), nullable=True)
+
     places = relationship(
             "Place", backref="user", cascade="all, delete-orphan")
