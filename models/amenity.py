6d7b9b38beb20eebfd706b0abe0c9b327b0714f1
jembantonio <jembantonio@gmail.com>
2019-08-15 23:24:40 +0000

    updated amenity to have relationship with place and added amenity to db storage list

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """This is the amenity class"""
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel):
     """This is the class for Amenity
     Attributes:
         name: input name
     """
-    name = ""
+
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+
+    place_amenities = relationship(
+            'Place', backref='Amenity', cascade='all, delete-orphan')
