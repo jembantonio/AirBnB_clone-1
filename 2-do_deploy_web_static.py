4f82e637e5656b14d658870b04a7328867265517
jembantonio <jembantonio@gmail.com>
2019-08-20 06:32:46 +0000

    fab script that distributes archive to web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+''' a Fabric script that distributes an archive to your web servers
+'''
+from fabric.api import *
+
+
+def do_deploy(archive_path):
+    ''' takes the archive path and distributes the given archive to webservers
+    '''
+
+    env.hosts = ["35.229.110.107", "35.196.24.51"]
+
+    path = "/data/web_static/releases/{}/".format(name)
+    name = filename.split('.')[0]
+    currentpath = /data/web_static/current
+
+    if not exists(archive_path):
+        return false
+    try:
+        put(archive_path, "/tmp/")
+        run("rm -rf /data/web_static/releases/{}/".format(
+            (archive_path.split('/')[-1]).split(".")[0]))
+        run("mkdir -p {}".format(path))
+        run("tar -xzf /tmp/{} -C {}".format(
+            (archive_path.split('/')[-1]), path))
+        run("rm /tmp/{}".format(archive_path.split('/')[-1]))
+        run("mv {}web_static/* {}".format(path, path))
+        run("rm -rf {}web_static".format(path))
+        run("rm -rf {}".format(currentpath)
+        run("ln -s /data/web_static/releases/{}/ {}".format(
+            ((archive_path.split('/')[-1]).split(".")[0]), currentpath)
+        return True
+
+    except:
+        return False
