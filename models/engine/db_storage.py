3318a3f0b179a1421943db0745c787ed5294519f
jembantonio <jembantonio@gmail.com>
2019-09-03 17:42:56 +0000

    changed state model to append a new city list

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -72,4 +72,4 @@ class DBStorage:
         self.__session = Session()
 
     def close(self):
-        self.__session.remove()
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -19,14 +19,15 @@ class State(BaseModel, Base):
     cities = relationship('City',
                           cascade='all, delete-orphan', backref='State')
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        name = ""
         @property
         def cities(self):
             """ getter for cities that returns list of
                 city instances with state_id equal to current State.id
             """
             city_list = []
-            for city_search in self.cities:
+            for city_search in models.storage.all("City").values():
                 if city_search.state_id == self.id:
                     city_list.append(city_search)
             return(city_list)
